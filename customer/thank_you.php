d9053efa841edb20f03cc09a7bc2d462b092a0cb
Shekinah-Ntumba <smpinguyabo@gmail.com>
2024-07-12 15:07:39 +0300

    Create thank_you.php

---
+++ customer/thank_you.php
@@ -0,0 +1,10 @@
+<?php
+include('../Config/constant.php');
+include('partial/menu.php');
+?>
+    <h1>Thank you for your payment!</h1>
+    <p>Your payment has been successfully processed.</p><br><br>
+    <a href="index.php" class="btn btn-primary">Continue to shop</a>
+    <!-- Additional details or redirection links as needed -->
+   <?php  include('partial/footer.php');
+    ?>
