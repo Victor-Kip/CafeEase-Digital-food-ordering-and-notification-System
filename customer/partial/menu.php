d5b306c81072d8cad7842688b1853b640f47ae09
Shekinah-Ntumba <smpinguyabo@gmail.com>
2024-07-12 15:07:02 +0300

    Create menu.php

---
+++ customer/partial/menu.php
@@ -0,0 +1,56 @@
+<?php include('../Config/constant.php'); ?>
+<?php 
+include('login_check.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cafeteria Website</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+            <span>CafeEase</span>                
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="categories.php">Categories</a>
+                    </li>
+                    <li>
+                        <a href="foods.php">Foods</a>
+                    </li>
+                    <li>
+                        <a href="profile.php">My account</a>
+                    </li>
+                    <li>
+                        <a href="eWallet.php">E-wallet</a>
+                    </li>
+                    <li>
+                        <a href="cart.php">cart</a>
+                    </li>
+                    <li>
+                        <a href="logout.php">logout</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Navbar Section Ends Here -->
